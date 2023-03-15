85957a8318cd25ba75650eaf563180bce91a5cd7
thejavierCO <javier.cruz.odispo@hotmail.com>
2023-03-14 18:29:50 -0600

    created and using text input component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>Work in progress</div>
+  <div>Insert URL</div>
+  <Input @change="data"></Input>
+  <div>Data potcasts</div>
+  <div>Data episodes</div>
 </template>
 
 <script>
+import Input from "./components/inputUrl.vue"
+
 export default {
+  components: {
+    Input
+  },
   data() {
     return {}
   },
+  methods: {
+    data: (a) => {
+      console.log(a);
+    }
+  },
   created() {
     console.warn("load");
+  },
+  mounted(a) {
+    console.log(a, this);
   }
 };
 </script>
---
+++ src/components/inputUrl.vue
@@ -0,0 +1,12 @@
+<template>
+  <form action="#">
+    <input type="text" />
+    <input type="submit" value="Guardar">
+  </form>
+</template>
+
+<script>
+export default {
+  name: "inputURL"
+}
+</script>
