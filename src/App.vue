103e2b977a5c70a2e61c554a526420565d6f0bf7
Javier Cruz <31808656+thejavierCO@users.noreply.github.com>
2024-05-05 22:58:18 +0000

    add dark theme

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <van-nav-bar :title="Title" />
-  <van-grid :column-num="1" :gutter="10">
-    <van-grid-item>
-      <h3>{{ potCast.title }}</h3>
-      <img :src="potCast.image" width="300px"/>
-    </van-grid-item>
-  </van-grid>
-  <van-grid :column-num="4" :gutter="10">
-    <van-grid-item v-for="item in potCast.items" :key="item">
-      <h4>{{ item.title }} - {{ item.author }}</h4>
-      <h5>{{ item.description }}</h5>
-      <div v-if="item.enclosures[0].url">
-        <audio :src="item.enclosures[0].url" controls></audio>
-      </div>
-      <div v-else>
-        not audio
-      </div>
-    </van-grid-item>
-  </van-grid>
+  <van-config-provider theme="dark">
+    <van-nav-bar :title="Title" />
+    <van-grid :column-num="1" :gutter="100">
+      <van-grid-item>
+        <h3>{{ potCast.title }}</h3>
+        <img :src="potCast.image" width="300px"/>
+      </van-grid-item>
+    </van-grid>
+    <van-grid :column-num="4" :gutter="10">
+      <van-grid-item v-for="item in potCast.items" :key="item">
+        <h4>{{ item.title }} - {{ item.author }}</h4>
+        <div v-html="item.description" />
+        <div v-if="item.enclosures[0].url">
+          <audio :src="item.enclosures[0].url" controls preload="none"></audio>
+        </div>
+        <div v-else>
+          not audio
+        </div>
+      </van-grid-item>
+    </van-grid>
+  </van-config-provider>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import { NavBar } from 'vant';
 import App from './App.vue';
-import { Grid, GridItem } from 'vant';
+import { Grid, GridItem, ConfigProvider } from 'vant';
 import 'vant/lib/index.css';
+import './style.css';
 
-const app = createApp(App,{Title:"personal app"})
+const app = createApp(App, { Title: "personal app" })
 
-app.use(NavBar).use(Grid).use(GridItem)
+app.use(NavBar).use(Grid).use(GridItem).use(ConfigProvider)
 
 app.mount("#app")
